e50257599b4006dbe2465c458fd192e41d0c4220
Andy Vanek <andy_vanek@trimble.com>
2023-05-04 15:10:22 -0400

    Implement use of OidcSecurityService to initiate the TID OIDC sign-in and sign-out grant flows.

--- Angular/src/app/auth/tidauth/tidauth.component.ts
+++ Angular/src/app/auth/tidauth/tidauth.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginResponse, OidcSecurityService } from 'angular-auth-oidc-client';
+import { Observable, take } from 'rxjs';
 
 @Component({
   selector: 'app-tidauth',
   templateUrl: './tidauth.component.html',
   styleUrls: ['./tidauth.component.scss']
 })
-export class TIDAuthComponent {
+export class TIDAuthComponent implements OnInit {
 
+  authCheck$: Observable<LoginResponse> | undefined;
+
+  public constructor(private oidcSvc: OidcSecurityService) {}
+  
+  ngOnInit(): void {
+    // This will ensure the code -> token exchange occurs
+    this.authCheck$ = this.oidcSvc.checkAuth();
+  }
+
+  public signin(): void {
+    // This performs the call to TID to do the grant type
+    // flow up until the code -> token exchange
+    this.oidcSvc.authorize();
+  }
+
+  public signout(): void {
+    // This library requires you to subscribe to the service method
+    // for it to actually execute the logoff logic. The pipe/take
+    // is an rxjs workaround for the subscribe call to not require
+    // a call to unsubscribe.
+    this.oidcSvc.logoff().pipe(take(1)).subscribe({ next: () => {}});
+  }
 }
