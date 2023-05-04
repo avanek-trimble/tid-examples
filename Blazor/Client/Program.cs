b3fe894c37c2ab92d9a07ee7aab56e424cf42d1b
Andy Vanek <andy_vanek@trimble.com>
2023-05-04 12:24:33 -0400

    Add TID OIDC authentication to Blazor client project

--- Blazor/Client/Program.cs
+++ Blazor/Client/Program.cs
@@ -8,4 +8,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddOidcAuthentication(o =>
+{
+    // remove the default scopes provided by the NuGet package
+    o.ProviderOptions.DefaultScopes.Clear();
+
+    // read the settings from the appsettings.json file
+    builder.Configuration.Bind("TrimbleIdentity4", o.ProviderOptions);
+});
+
 await builder.Build().RunAsync();
