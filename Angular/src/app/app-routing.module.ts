3e7e4243e161c8835bb18796e72726beb0665ebe
Andy Vanek <andy_vanek@trimble.com>
2023-05-04 08:47:02 -0400

    Pare down placeholder code in app component, add Angular Material library, create basic heaver nav.

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{ path: 'weather', loadChildren: () => import('./weather-forecast/weather-forecast.module').then(m => m.WeatherForecastModule) }];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'weather', loadChildren: () => import('./weather-forecast/weather-forecast.module').then(m => m.WeatherForecastModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
