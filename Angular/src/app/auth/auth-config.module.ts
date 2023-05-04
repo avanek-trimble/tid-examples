623c49e73e3daffaa0b5063f7ffdbf5bb8029df6
Andy Vanek <andy_vanek@trimble.com>
2023-05-04 15:10:22 -0400

    Implement use of OidcSecurityService to initiate the TID OIDC sign-in and sign-out grant flows.

--- Angular/src/app/auth/auth-config.module.ts
+++ Angular/src/app/auth/auth-config.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { AuthModule } from 'angular-auth-oidc-client';
-import { TIDAuthComponent } from './tidauth/tidauth.component';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
+import { TIDAuthComponent } from './tidauth/tidauth.component';
 
 @NgModule({
     imports: [AuthModule.forRoot({
@@ -10,13 +12,16 @@ import { TIDAuthComponent } from './tidauth/tidauth.component';
               redirectUrl: window.location.origin,
               postLogoutRedirectUri: window.location.origin,
               clientId: '4ce6f16c-f0c2-4c86-8388-db37511df6c1',
-              scope: 'openid AV-TID-Example',
+              scope: 'openid AV-TID-Example offline_access',
               responseType: 'code',
               silentRenew: true,
               useRefreshToken: true,
               renewTimeBeforeTokenExpiresInSeconds: 30,
           }
-      })],
+      }),
+      CommonModule,
+      MatButtonModule
+    ],
     exports: [ TIDAuthComponent, AuthModule],
     declarations: [
       TIDAuthComponent
