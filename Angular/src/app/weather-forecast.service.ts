3e8aa7059365516137b5db35346f5a7167daf055
Andy Vanek <andy_vanek@trimble.com>
2023-05-08 12:11:59 -0400

    Added TID bearer token to HttpClient call to backend API

--- Angular/src/app/weather-forecast.service.ts
+++ Angular/src/app/weather-forecast.service.ts
@@ -1,16 +1,28 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, map, switchMap } from 'rxjs';
 import { WeatherForecast } from './models/weather-forecast';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherForecastService {
 
-  constructor(private httpClient: HttpClient ) { }
+  constructor(private httpClient: HttpClient, private oidcSvc: OidcSecurityService) { }
+
+  private getAuthorizationHeader(): Observable<string> {
+    return this.oidcSvc.getAccessToken().pipe(
+      map(accessToken => (`Bearer ${accessToken}`))
+    );
+  }
 
   getWeatherForecast(): Observable<WeatherForecast[]> {
-    return this.httpClient.get<WeatherForecast[]>('https://localhost:7227/WeatherForecast');
+    return this.getAuthorizationHeader().pipe(
+      switchMap(auth => {
+        const httpHeaders = new HttpHeaders().set('Authorization', auth);
+        return this.httpClient.get<WeatherForecast[]>('https://localhost:7227/WeatherForecast', { headers: httpHeaders });
+      })
+    );
   }
 }
