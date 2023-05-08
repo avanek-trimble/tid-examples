e0cd8e461c88c2b23ed02b47210b7ac15bb84b14
Andy Vanek <andy_vanek@trimble.com>
2023-05-08 11:48:07 -0400

    Add TID bearer token to all "BackendAPI" HttpClient requests in Blazor Client application.

--- Blazor/Client/Program.cs
+++ Blazor/Client/Program.cs
@@ -1,12 +1,19 @@
 using Blazor.Client;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services
+    .AddHttpClient("BackendAPI", c => c.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>(); // adds TID JWT to Authorization header
+
+// use the BackendAPI configured HttpClient any time one is injected into a Razor page/component.
+builder.Services.AddScoped(s => s.GetRequiredService<IHttpClientFactory>().CreateClient("BackendAPI"));
+
 
 builder.Services.AddOidcAuthentication(o =>
 {
